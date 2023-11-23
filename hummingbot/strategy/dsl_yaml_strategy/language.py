247936665304681dfddd7458728b91eedcb0b727
christos99 <mangas.99@windowslive.com>
2023-11-23 16:56:56 +0200

    new language.py and a demo_strategy.yaml which work very nicely

--- hummingbot/strategy/dsl_yaml_strategy/language.py
+++ hummingbot/strategy/dsl_yaml_strategy/language.py
@@ -1,10 +1,14 @@
 import yaml
 
 class Strategy:
-    def __init__(self, name, version, type, markets, parameters):
+    def __init__(self, name, version, type, author, author_email, description, labels, markets, parameters):
         self.name = name
         self.version = version
         self.type = type
+        self.author = author
+        self.author_email = author_email
+        self.description = description
+        self.labels = labels
         self.markets = markets
         self.parameters = parameters
 
@@ -24,13 +28,23 @@ class Market:
         print(f"Market: {self.connector}, Pairs: {self.pairs}")
 
 class Parameter:
-    def __init__(self, name, type, default):
+    def __init__(self, name, type, description, prompt_msg, default, keyword, dynamic_reconfigure, prompt_on_new):
         self.name = name
         self.type = type
+        self.description = description
+        self.prompt_msg = prompt_msg
         self.default = default
+        self.keyword = keyword
+        self.dynamic_reconfigure = dynamic_reconfigure
+        self.prompt_on_new = prompt_on_new
 
     def display(self):
         print(f"Parameter: {self.name}, Type: {self.type}, Default: {self.default}")
+
+class ExchangeConfig:
+    def __init__(self, apiKey, apiSecret):
+        self.apiKey = apiKey
+        self.apiSecret = apiSecret
 
 def load_yaml(file_path):
     try:
@@ -44,20 +58,21 @@ def load_yaml(file_path):
         return None
 
 def parse_strategy(data):
-    if not data or 'markets' not in data or 'parameters' not in data:
+    if not data:
         print("Invalid strategy data format")
         return None
 
-    markets = [Market(m['connector'], m['pairs']) for m in data['markets']]
-    parameters = [Parameter(p['name'], p['type'], p['default']) for p in data['parameters']]
-    return Strategy(data['name'], data['version'], data['type'], markets, parameters)
+    markets = [Market(m['market']['connector'], m['market']['pairs']) for m in data.get('markets', [])]
+    parameters = [Parameter(p['parameter']['name'], p['parameter']['type'], p['parameter']['description'], p['parameter']['prompt_msg'], p['parameter']['default'], p['parameter']['keyword'], p['parameter']['dynamic_reconfigure'], p['parameter']['prompt_on_new']) for p in data.get('parameters', [])]
+
+    return Strategy(data['name'], data['version'], data['type'], data.get('author', ''), data.get('author_email', ''), data.get('description', ''), data.get('labels', []), markets, parameters)
 
 def main():
-    yaml_data = load_yaml('strategy.yaml')  # Adjust the file name as needed
+    yaml_data = load_yaml('/Users/christos/hummingbot-dsl/hummingbot/strategy/dsl_yaml_strategy/demo_strategy.yaml')  # Replace with your actual file path
     if yaml_data:
         strategy = parse_strategy(yaml_data['strategy_model'])
         if strategy:
-            strategy.display()
+            strategy.display()  # Replace with your desired processing or output format
 
 if __name__ == '__main__':
     main()
